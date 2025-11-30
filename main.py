40a45fa5b0582968c5626e2f1d7fc6e819661487
allenrrob <allenrrob@gmail.com>
2025-11-30 14:27:21 -0800

    capped framrate to 60 fps

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -18,6 +20,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
