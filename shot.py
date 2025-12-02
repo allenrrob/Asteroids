6f2a9dc951e0ef9b05808fdccf3f26c49da4557f
allenrrob <allenrrob@gmail.com>
2025-12-01 21:27:11 -0800

    fixed shooting

--- shot.py
+++ shot.py
@@ -6,8 +6,8 @@ class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
 
-        def draw(self, screen):
-            pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius)
         
-        def update(self, dt):
-            self.position += self.velocity * dt
+    def update(self, dt):
+        self.position += self.velocity * dt
